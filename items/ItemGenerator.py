9967d629ee45ab115e4f12478ec299e37e627061
chunzhi23 <lzhongm05@gmail.com>
2024-02-21 16:07:39 +0900

    follow main branch at that time

--- items/ItemGenerator.py
+++ items/ItemGenerator.py
@@ -8,4 +8,4 @@ class ItemGenerator:
         length = len(self.item_list)
         randInt = random.randint(0, length)
         selectItem = self.item_list[randInt]
-        # 아이템 랜덤하게 선택 후 함수 실행. pos는 좌표값
+        # 아이템 랜덤하게 선택 후 함수 실행. pos는 좌표값.
