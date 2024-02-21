af40634e381f780a6b629f5c3707dbdf87a9c17d
jsk <83943310+jsk611@users.noreply.github.com>
2024-02-21 15:48:05 +0900

    Update Item.py

--- items/Item.py
+++ items/Item.py
@@ -2,18 +2,19 @@ import pygame
 import random
 
 class Item:
-    def __init__(self, frame, type):
+    def __init__(self, frame, type, color):
         self.position = [random.randrange(1, (frame[0]//10)) * 10,
                              random.randrange(1, (frame[1]//10)) * 10] #[x,y] 위치
         self.type = type # 아이템 종류
         self.spawn_time = pygame.time.get_ticks()
+        self.color = color
 
     def update_timer(self):
         # 타이머 업데이트 메서드 - 폭탄 아이템이면 app.py에서 실행
         current_time = pygame.time.get_ticks()  # 현재 시간 가져오기
         elapsed_time = current_time - self.spawn_time  # 경과된 시간 계산
 
-        if elapsed_time >= 30000:  # 30초 지나면
+        if elapsed_time >= 5000:  # 30초 지나면
             #폭발
             print("폭발함")
     
